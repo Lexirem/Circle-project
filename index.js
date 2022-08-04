32f4d06a72de05cb2fcc45d39f81a71c090b79c8
Txell <meritxell@gold.barcelona>
2022-08-04 19:52:47 +0200

    redirect to contact page

---
+++ index.js
@@ -0,0 +1,13 @@
+// Contact Page JS
+
+
+
+// Project Page JS
+// function project(){
+//   document.getElementsByClassName("card").addEventListener("click")
+// }
+// var params = new Proxy (new URLSearchParams(window.location.href=`127.0.0.1:5500/project.html?title=${title}&subtitle=${subtitle}`), {
+//   get: (searchParams, prop) => searchParams.get(prop)
+// })
+// const projectTitle = params.title;
+// const projectSubtitle = params.subtitle;
