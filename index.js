4e19c250086fd185219f366dbafe48621c4cc7b6
Txell <meritxell@gold.barcelona>
2022-08-04 21:15:57 +0200

    form js done

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 // Contact Page JS
+window.onload = () => {
+  document.getElementById("submit").addEventListener("click", submitForm);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  let user = document.getElementById('fname').value;
+  if (user.length == 0){
+    alert('Please, enter your name');
+    return;
+  };
+  let email = document.getElementById('email').value;
+  if (email ==''){
+    alert('Please, enter your email')
+  };
+  let phone = document.getElementById('tel').value;
+  if (phone.length >= 1 && phone.length < 9){
+    alert('Not enough numbers');
+    return;
+  };
+  if (submitForm == ''){
+    alert('Please fill the formulary');
+  } else {
+    alert(`Thanks, ${user} for getting in touch with us, we'll contact you as soon as possible!!`);
+  };
+  this.submit();
+};
+
 
 
 
